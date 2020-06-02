26b48efec5cc7e02605ac46cdd442946ed8888d5
Yas <bartosz12364@gmail.com>
2020-06-02 22:08:57 +0200

    Add arg parsing to generate_db.py

--- generate_db.py
+++ generate_db.py
@@ -3,15 +3,19 @@ from sqlalchemy.orm import sessionmaker
 
 import models
 import json
+import argparse
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", type=str, help="Input .json file")
+    args = parser.parse_args()
     engine = create_engine('sqlite:///sqlalchemy.sqlite', echo=True)
     models.Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    with open('76kPLfixed5x.json', encoding="utf-8-sig") as f:
+    with open(args.input, encoding="utf-8-sig") as f:
         dataset = json.load(f)
 
     for data in dataset:
